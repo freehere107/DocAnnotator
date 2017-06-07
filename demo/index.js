4dbb602d5d3542f601f6d2d089644fb33251e7ea
xiewei <cyerogy@163.com>
2017-06-07 14:15:03 +0800

    添加交互

--- demo/index.js
+++ demo/index.js
@@ -1,56 +1,29 @@
-/* Copyright 2014 Mozilla Foundation
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-'use strict';
-
-if (!PDFJS.PDFViewer || !PDFJS.getDocument) {
-  alert('Please build the pdfjs-dist library using\n' +
-        '  `gulp dist`');
+PDFJS.workerSrc = 'build/dist/build/pdf.worker.js';
+var DEFAULT_URL = 'compressed.tracemonkey-pldi-09.pdf';
+var SCALE = 1.0;
+var container = document.getElementById('pageContainer');
+function getContextByPage(page){
+    container.innerHTML = "";
+    page = parseInt(page);
+    PDFJS.getDocument(DEFAULT_URL).then(function (pdfDocument) {
+        document.getElementById('allpages').innerHTML = pdfDocument.numPages;
+        pdfDocument.getPage(page).then(function (pdfPage) {
+            var pdfPageView = new PDFJS.PDFPageView({
+                container: container,
+                id: page,
+                scale: SCALE,
+                defaultViewport: pdfPage.getViewport(SCALE),
+                textLayerFactory: new PDFJS.DefaultTextLayerFactory(),
+                annotationLayerFactory: new PDFJS.DefaultAnnotationLayerFactory()
+            });
+            pdfPageView.setPdfPage(pdfPage);
+            pdfPageView.draw();
+        });
+    });
+}
+function jumpPage(){
+    var page = document.getElementById('page').value;
+    getContextByPage(page);
 }
 
-// The workerSrc property shall be specified.
-//
-PDFJS.workerSrc = 'build/dist/build/pdf.worker.js';
-
-// Some PDFs need external cmaps.
-//
-// PDFJS.cMapUrl = '../../build/dist/cmaps/';
-// PDFJS.cMapPacked = true;
-
-var DEFAULT_URL = 'compressed.tracemonkey-pldi-09.pdf';
-var PAGE_TO_VIEW = 1;
-var SCALE = 1.0;
-
-var container = document.getElementById('pageContainer');
-
-// Loading document.
-PDFJS.getDocument(DEFAULT_URL).then(function (pdfDocument) {
-  // Document loaded, retrieving the page.
-  return pdfDocument.getPage(PAGE_TO_VIEW).then(function (pdfPage) {
-    // Creating the page view with default parameters.
-    var pdfPageView = new PDFJS.PDFPageView({
-      container: container,
-      id: PAGE_TO_VIEW,
-      scale: SCALE,
-      defaultViewport: pdfPage.getViewport(SCALE),
-      // We can enable text/annotations layers, if needed
-      textLayerFactory: new PDFJS.DefaultTextLayerFactory(),
-      annotationLayerFactory: new PDFJS.DefaultAnnotationLayerFactory()
-    });
-    // Associates the actual page with the view, and drawing it
-    pdfPageView.setPdfPage(pdfPage);
-    return pdfPageView.draw();
-  });
-});
+getContextByPage(1);
